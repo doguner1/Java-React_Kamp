0f85c46f7e7abf950029e413611d98b36905c47b
D O Ğ U <d.gunr@yaani.com>
2022-09-08 15:12:43 +0300

    Add project files.

--- inheritance2/src/inheritance2/Main.java
+++ inheritance2/src/inheritance2/Main.java
@@ -4,8 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Logger emailLogger = new EmailLogger();
 		CustomerManager customerManager = new CustomerManager();
 		customerManager.Add(new DataBaseLogger());
+		customerManager.Add(new FileLogger());
+		customerManager.Add(emailLogger);
 
 	}
 
